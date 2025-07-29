0ad25c2100b9c66886b0fbe01619687908e3e7cd
mr_JaimePerez <jaime43lenin@gmail.com>
2025-07-28 20:57:01 -0400

    Revert "Revert "update_login_and_remote_db""
    This reverts commit ae97d76b681bbad6905fd63af189a73e6c437ed5.

--- campus_cart/lib/db/stalls_service.dart
+++ campus_cart/lib/db/stalls_service.dart
@@ -1,38 +1,9 @@
 import '../models/stall.dart';
-import '../models/product.dart';
 import 'supabase_service.dart';
 
 class StallsService {
   static Future<List<Stall>> getStalls() async {
     final res = await SupabaseService.client.from('stalls').select();
     return res.map<Stall>((e) => Stall.fromMap(e)).toList();
-  }
-
-  static Future<List<Product>> getProducts(int stallId) async {
-    final res = await SupabaseService.client
-        .from('menu_items')
-        .select()
-        .eq('stall_id', stallId);
-    return res.map<Product>((e) => Product.fromMap(e)).toList();
-  }
-
-  static Future<void> addProduct(
-    int stallId,
-    String name,
-    String description,
-    double price,
-    int stock,
-    bool shippable, {
-    String? imageUrl,
-  }) async {
-    await SupabaseService.client.from('menu_items').insert({
-      'stall_id': stallId,
-      'name': name,
-      'description': description,
-      'price': price,
-      'stock': stock,
-      'shippable': shippable,
-      'image_url': imageUrl,
-    });
   }
 }
